282ceb153a241afb95efd94424b4d7d19abe7f95
Rabindra Joshi <mail@rjoshi.net>
2020-01-03 23:57:35 +0545

    Add env file.

---
+++ env.example.sh
@@ -0,0 +1,11 @@
+remoteUser='root'
+remoteIp='11.111.11.111'
+remotePath='/var/www/example.com/htdocs/tea/'
+
+# Will ask for confirmation on retrying to run script within this time
+# No will still upload file to server, but won't update history/count
+minRepeatInMinutes=5
+# Only entries ending in currentYear are counted!
+currentYear=2020
+# Enable/disable rsync. 1 => true, everything else => false
+pushToRemote=1

573dd86d7763612b2aee18787508490e1f2037d8
Rabindra Joshi <mail@rjoshi.net>
2020-01-26 21:13:44 +0545

    allow more conditions

--- env.example.sh
+++ env.example.sh
@@ -1,11 +1,32 @@
+#####################
+# General Settings
+#####################
+# will ask for confirmation if repeated within this minutes.
+minRepeatInMinutes=70
+# current year => default count
+currentYear=2020
+# drink name... tea or anything else!
+drinkName='tea'
+# unit for the drink...
+unitName='cup'
+
+#####################
+# FLAGS
+#####################
+# enable commit = 1
+gitCommit=1
+# enable git push = 1
+gitPush=1
+# Enable/disable rsync. enable=1
+rsyncToRemote=0
+
+#####################
+# RSYNC Settings
+#####################
+# rsync user
 remoteUser='root'
+# rsync ip
 remoteIp='11.111.11.111'
+# rsync path
 remotePath='/var/www/example.com/htdocs/tea/'
 
-# Will ask for confirmation on retrying to run script within this time
-# No will still upload file to server, but won't update history/count
-minRepeatInMinutes=5
-# Only entries ending in currentYear are counted!
-currentYear=2020
-# Enable/disable rsync. 1 => true, everything else => false
-pushToRemote=1

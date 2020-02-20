142627b6208ebb1b44a8202c4bc24fed4d26b9b4
Rabindra Joshi <mail@rjoshi.net>
2020-02-20 12:39:36 +0545

    pull before adding a drink

--- drink.sh
+++ drink.sh
@@ -1,115 +1,98 @@
 #!/bin/bash
 
-
-drinkTea(){
+drinkTea() {
   # Write to history
   appDir=$(dirname "${0}")
   source $appDir/env.sh
-  
+  cd $appDir && git pull 1>/dev/null 2>&1 && cd -
+
   # --nocommit --nopush --norsync
   # --no = No to everything
   # no commit also means no push
-  
+
   handle_flags $@
-  
+
   historyFile="$appDir/history.txt"
   secondLastLine=''
   lastLine=''
-  
-  
-  while IFS= read -r line
-  do
+
+  while IFS= read -r line; do
     secondLastLine=$lastLine
     lastLine=$line
-  done < "$historyFile"
-  
-  if [[ -z $secondLastLine &&  -z "$lastLine" ]]
-  then
+  done <"$historyFile"
+
+  if [[ -z $secondLastLine && -z "$lastLine" ]]; then
     echo "Could not locate last date"
     lastDateValid=0
     # exit 1
   else
     lastDateValid=1
   fi
-  
-  if [ -z "$lastLine" ]
-  then
+
+  if [ -z "$lastLine" ]; then
     lastLine=$secondLastLine
   fi
-  
+
   day=${lastLine%% *}
   currentDateTime=$(date)
   today=${currentDateTime%% *}
-  
-  
+
   lastTimestamp=0
   currentTimestamp=$(date -d "$currentDateTime" +%s)
-  
-  if [[ lastDateValid -eq 1 ]]
-  then
+
+  if [[ lastDateValid -eq 1 ]]; then
     lastTimestamp=$(date -d "$lastLine" +%s)
   fi
-  
-  
-  if [[ $day != $today ]]
-  then
+
+  if [[ $day != $today ]]; then
     echo "Inserting new line"
-    echo -e "" >> $historyFile
+    echo -e "" >>$historyFile
   fi
-  
-  
+
   distanceFromLast=$((currentTimestamp - lastTimestamp))
-  
+
   again='y'
-  
-  if [[ distanceFromLast -lt $((minRepeatInMinutes * 60)) ]]
-  then
+
+  if [[ distanceFromLast -lt $((minRepeatInMinutes * 60)) ]]; then
     read -n 99 -p "Drank again under $minRepeatInMinutes minutes? yes/no:" again
   fi
-  
+
   again="${again,,}" #Lowercase
-  
-  
-  if [[ $again == 'y' || $again == 'yes' ]]
-  then
+
+  if [[ $again == 'y' || $again == 'yes' ]]; then
     again='y'
     # Increasae Count
-    if [[ $doCount == 1 ]]
-    then
+    if [[ $doCount == 1 ]]; then
       echo "$currentDateTime"
-      echo "$currentDateTime" >> $historyFile
+      echo "$currentDateTime" >>$historyFile
       # Use timestamp instead/as well!
       # echo "$(date -d "$currentDateTime" +%s)" >> $historyFile
-      
+
     fi
   fi
-  totalCount=$(grep -o $currentYear $historyFile | wc -l )
-  echo "$totalCount" > "$appDir/index.html"
-  
+  totalCount=$(grep -o $currentYear $historyFile | wc -l)
+  echo "$totalCount" >"$appDir/index.html"
+
   echo "Total: $totalCount"
-  
-  if [[ $again == 'y' ]]
-  then
-    if [[ rsyncToRemote -eq 1 ]]
-    then
+
+  if [[ $again == 'y' ]]; then
+    if [[ rsyncToRemote -eq 1 ]]; then
       echo "**Running rsync!**"
       rsync -aqz $appDir/index.html $remoteUser@$remoteIp:$remotePath
       # Add more rsync here > history?
     fi
-    if [[ gitCommit -eq 1 ]]
-    then
+    if [[ gitCommit -eq 1 ]]; then
       # Have a specific branch? > checkout here
       cd $appDir
       git add index.html
       # Add a history file as well, if you're comfortable making it public (watch gitignore)
-      
+
       # 1st, 2nd, 3rd or 4th. Handle the ordinal (th)
       ordinalChar='th' # default, just in case
       setOrdinalChar $totalCount
-      
+
       git commit --quiet -m "${totalCount}$ordinalChar $unitName of $drinkName in $currentYear" $appDir/index.html
-      if [[ gitPush -eq 1 ]]
-      then
+      if [[ gitPush -eq 1 ]]; then
         echo "**Pushing to git!**"
         git push --quiet
       fi
@@ -118,7 +101,6 @@ drinkTea(){
     echo "Not updated"
   fi
 }
-
 
 has_param() {
   local term="$1"
@@ -131,44 +113,44 @@ has_param() {
   return 1
 }
 
-handle_flags(){
+handle_flags() {
   if has_param '--no' "$@"; then
     echo "Nothing"
     rsyncToRemote=0
     gitCommit=0
     gitPush=0
   fi
-  
+
   if has_param '--nocommit' "$@"; then
     echo "No git commit"
     gitCommit=0
-    
+
   fi
-  
+
   if has_param '--nopush' "$@"; then
     gitPush=0
     echo "No git push"
   fi
-  
+
   if has_param '--norsync' "$@"; then
     rsyncToRemote=0
     echo "No rsync"
   fi
-  
+
   if has_param '--nocount' "$@"; then
     doCount=0
     echo "No count"
   fi
-  
+
 }
 
-setOrdinalChar(){
+setOrdinalChar() {
   # sets st, nd, rd, th for the numbers
   case "$1" in
-    *1[0-9] | *[04-9]) ordinalChar="th";;
-    *1) ordinalChar="st";;
-    *2) ordinalChar="nd";;
-    *3) ordinalChar="rd";;
+  *1[0-9] | *[04-9]) ordinalChar="th" ;;
+  *1) ordinalChar="st" ;;
+  *2) ordinalChar="nd" ;;
+  *3) ordinalChar="rd" ;;
   esac
 }
 
